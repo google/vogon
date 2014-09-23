8a6ba4b8760352ab76872e91d9275d1d5ab33ec3
Cristiano Breuel <cristianob@google.com>
2014-09-23 11:28:35 -0300

    Updated header and README.

--- README.md
+++ README.md
@@ -9,4 +9,8 @@ position and font definitions for each piece of text and image, referencing
 data fields through their names. Fixed text can also be used in the layout
 specification.
 
+The generated videos are (optionally) uploaded to a Youtube channel, and a 
+campaign specification file is generated to be imported in AdWords for Video,
+creating geo-targeted campaigns for each of the videos.
+
 This is not an official Google product.
--- vogon.py
+++ vogon.py
@@ -25,6 +25,10 @@ The data can contain text and images. The specification determines the timing,
 position and font definitions for each piece of text and image, referencing
 data fields through their names. Fixed text can also be used in the layout
 specification.
+
+The generated videos are (optionally) uploaded to a Youtube channel, and a
+campaign specification file is generated to be imported in AdWords for Video,
+creating geo-targeted campaigns for each of the videos.
 """
 
 import argparse
